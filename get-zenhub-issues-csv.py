f4bfed2af755f869b697fa9b32e7df6fcd06df4d
Levi Lugato <levilugato@Levis-MacBook-Pro.local>
2023-06-21 12:26:11 -0300

    fix pagination and get all issues same request

--- get-zenhub-issues-csv.py
+++ get-zenhub-issues-csv.py
@@ -19,70 +19,72 @@ query work($workspace_id: ID!) {
         name
         description
         id   
-        }     
-      }
+      }     
     }
   }
+}
 '''
 
 # Define the GraphQL query for issues
 query_issues = '''
-query ($pipelineId: ID!, $workspaceId: ID!, $filters: IssueSearchFiltersInput!) {
-    searchIssuesByPipeline(pipelineId: $pipelineId, filters: $filters) {
-        nodes {
-            id
-            title
-            number
-            body
-            state
-            createdAt
-            updatedAt
-            repository{
-                name
-                id
-            }
-            user{
-                login
-            }
-            pipelineIssue(workspaceId: $workspaceId) {
-                pipeline {
-                    id
-                    name
-                }
-            }
-            parentZenhubEpics(first:10){
-                nodes {
-                    title
-                }
-            }
-            assignees(first:10){
-                nodes {
-                    name
-                }
-            }
-            estimate{
-                value
-                }
-            sprints(first: 10) {
-      nodes {
-        id
-        name
-      }
+query ($pipelineId: ID!, $workspaceId: ID!, $filters: IssueSearchFiltersInput!, $after: String) {
+  searchIssuesByPipeline(pipelineId: $pipelineId, filters: $filters, after: $after) {
+    pageInfo {
+      endCursor
+      hasNextPage
     }
-    labels(first: 10) {
-      nodes {
-        id
+    nodes {
+      id
+      title
+      number
+      body
+      state
+      createdAt
+      updatedAt
+      repository {
         name
-        color
+        id
       }
-    }
+      user {
+        login
+      }
+      pipelineIssue(workspaceId: $workspaceId) {
+        pipeline {
+          id
+          name
         }
+      }
+      parentZenhubEpics(first: 10) {
+        nodes {
+          title
+        }
+      }
+      assignees(first: 10) {
+        nodes {
+          name
+        }
+      }
+      estimate {
+        value
+      }
+      sprints(first: 10) {
+        nodes {
+          id
+          name
+        }
+      }
+      labels(first: 10) {
+        nodes {
+          name
+        }
+      }
     }
+  }
 }
 '''
 
-def set_payload(query):
-    return  {
+def set_payload(query, variables):
+    return {
         "query": query,
         "variables": variables
     }
@@ -100,43 +102,56 @@ variables = {
     "workspace_id": workspace_id
 }
 
-response_pipelines = requests.post(url, headers=headers, json=set_payload(query_pipelines))
-data_pipelines = response_pipelines.json()
+try:
+    response_pipelines = requests.post(url, headers=headers, json=set_payload(query_pipelines, variables))
+    data_pipelines = response_pipelines.json()
 
-pipeline_ids = [node['id'] for node in data_pipelines['data']['workspace']['pipelinesConnection']['nodes']]
+    pipeline_ids = [node['id'] for node in data_pipelines['data']['workspace']['pipelinesConnection']['nodes']]
 
-for pipeline_id in pipeline_ids:
+    for pipeline_id in pipeline_ids:
+        variables = {
+            "workspaceId": workspace_id,
+            "pipelineId": pipeline_id,
+            "filters": {},
+            "after": None
+        }
 
-    print(pipeline_id)
+        page_counter = 1  # Counter for generating unique filenames
 
-    variables = {
-        "workspaceId": workspace_id,
-        "pipelineId": pipeline_id,
-        "filters": {}
-    }
+        while True:
+            response = requests.post(url, headers=headers, json=set_payload(query_issues, variables))
+            try:
+                data = json.loads(response.content)
 
-    # Send the GraphQL request
-    response = requests.post(url, headers=headers, json=set_payload(query_issues))
-    data = response.json()
+                nodes = data['data']['searchIssuesByPipeline']['nodes']
 
-    try:
-        # Extract the nodes from the response data
-        nodes = data['data']['searchIssuesByPipeline']['nodes']
+                if not nodes:
+                    print(f"No issues found for pipeline ID: {pipeline_id}")
+                    break
 
-        if not nodes:
-            print(f"No issues found for pipeline ID: {pipeline_id}")
-            continue  # Skip to the next pipeline ID
+                pipelineName = data['data']['searchIssuesByPipeline']['nodes'][0]['pipelineIssue']['pipeline']['name']
+                pipelineName = pipelineName.replace('Bugs/', 'Bugs-')
 
-        pipelineName = data['data']['searchIssuesByPipeline']['nodes'][0]['pipelineIssue']['pipeline']['name']
-        
-        # Convert the nodes to a DataFrame
-        df = pd.json_normalize(nodes)
-        output_file = output_dir + f'{pipelineName}.csv'
+                df = pd.json_normalize(nodes)
+                df['labels_name'] = df['labels.nodes'].apply(lambda x: ', '.join([label['name'] for label in x]))
+                df.drop(columns=['labels.nodes'], inplace=True)
 
-        # Save the DataFrame as CSV
-        df.to_csv(output_file, index=False)
+                output_file = output_dir + f'{pipelineName}_{page_counter}.csv'  # Include page counter in the filename
+                df.to_csv(output_file, index=False, quoting=csv.QUOTE_NONNUMERIC)
+                print(f'Saved result to {output_file}')
 
-        print(f'Saved result to {output_file}')
+                pageInfo = data['data']['searchIssuesByPipeline']['pageInfo']
+                if pageInfo['hasNextPage']:
+                    variables['after'] = pageInfo['endCursor']
+                    page_counter += 1  # Increment the page counter
+                else:
+                    break
 
-    except KeyError:
-        print(f"No data found for pipeline ID: {pipeline_id}")
+            except (KeyError, json.JSONDecodeError) as e:
+                print(f"Error processing data for pipeline ID: {pipeline_id}")
+                print(e)
+                break
+
+except requests.RequestException as e:
+    print("Error occurred during the API request.")
+    print(e)
